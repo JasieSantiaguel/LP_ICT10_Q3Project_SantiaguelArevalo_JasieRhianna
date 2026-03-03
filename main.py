52d81abe1df21dddcc23d64e9d226930f914b564
JasieSantiaguel <santiaguel.jid@obmontessori.edu.ph>
2026-03-03 13:44:04 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+
+from js import document
+
+def check(event=None):
+    reg = document.querySelector('input[name="reg"]:checked')
+    med = document.querySelector('input[name="med"]:checked')
+    grade = document.getElementById("grade").value
+    section = document.getElementById("section").value
+
+    output_text = document.getElementById("output-text")
+    output_img = document.getElementById("output-img")
+
+    if reg is None or med is None:
+        output_text.innerText = "Please answer all questions"
+        output_img.src = ""
+
+    elif reg.value != "yes" or med.value != "yes":
+        output_text.innerText = "INELIGIBLE: Please make sure to register online and/or submit your medical clearance"
+        output_img.src = "red.jpg"
+
+    elif section == "sapphire":
+        output_text.innerText = f"G{grade} - Sapphire: Blue Bears. Congratulations!"
+        output_img.src = "blue.jpg"
+
+    elif section == "ruby":
+        output_text.innerText = f"G{grade} - Ruby: Red Bulldogs. Congratulations!"
+        output_img.src = "red.jpg"
+
+    elif section == "emerald":
+        output_text.innerText = f"G{grade} - Emerald: Green Hornets. Congratulations!"
+        output_img.src = "green.jpg"
+
+    elif section == "topaz":
+        output_text.innerText = f"G{grade} - Topaz: Yellow Tigers. Congratulations!"
+        output_img.src = "yellow.jpg"
+
+def sign_up():
+    confirmation = document.getElementById("confirm")
+
+    confirmation.innerText = "Account created. You may now log in using your credentials!"
+
+def show_players():
+    output_list = document.getElementById("output-list")
+    people = [
+
+   "Arevalo, Rhianna Mateo",
+"Atienza, Sebastian Caleb Bustamante",
+"Cubillas, Christian Emmanuel Lin",
+"David, Jolie Cassandra Marquez",
+"De Guzman, Stephanie Gaile",
+"De La Paz, Joaquin Matteo Altura",
+"De Leon, Marcus Theoden Catimpo",
+"De Luna, Andrew James Alipio",
+"Deinla, Brennan Shane Castillo",
+"Elevazo, John Rony Laurilla",
+"Francisco, Amber Ephesiah Manansala",
+"Ganal, Franceska Marie Salvador",
+"Lacerna, Atashya Khariz Coquilla",
+"Lavilla, Ava Samantha Gilo",
+"Luna, Shantia Angelic Ayong",
+"Morishita, John Ken Gudito",
+"Ortega, Megan Jacinth Capili",
+"Pangilinan, Noleen Kelly Berdejo",
+"Salonga, Carmen Beatriz Francisco",
+"Santiaguel, Jasie Grace Dejucos",
+"Solis, Brooke Anika Zamora",
+"Sta. Maria, John Gabriel Sanchez",
+"Tubangi, Lyle Yumi U.",
+"Urrutia, Alexander Lucas Prospero",
+"Valeroso, Eyl Carl Rante",
+"Villanueva, Art Varon", ]
+
+for i in range(len(people)):
+    print += f"{i+1}.) {people[i]}<br>"
